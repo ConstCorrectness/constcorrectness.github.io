23371de022783594c138d4c82cccd1b6182f25fb
horriblecpp <horribleccprogram@proton.me>
2025-07-17 23:58:40 -0400

    first layout init

---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+export const words = [
+  { text: 'Const', imgPath: '/images/ideas.svg'},
+  { text: 'Correctness', imgPath: '/images/concepts.svg'},
+  { text: 'Const', imgPath: '/images/code.svg'},
+  { text: 'Correctness', imgPath: '/images/designs.svg'}
+];
